d6f636a955982f6a5af9c18801094c28cb8315da
xieranmaya <xieranmaya@gmail.com>
2014-05-30 17:58:25 +0800

    add link, change color in fav-list

--- fav-list.js
+++ fav-list.js
@@ -12,43 +12,42 @@
 // 		}
 // 	}
 // });
-window.oncontextmenu = function(){return false}
-var FavListModule = angular.module("fav-list",[])
-.controller("favList",function($scope,$window,$location){
-	$scope.imgList = JSON.parse(localStorage.favList).reverse()
-	$scope.pageSize = 30//每页的数量
-	$scope.pageNum = parseInt($scope.imgList.length/$scope.pageSize+0.5)
-	$scope.currentPage = 1//$location.hash()||$location.hash(1)
-	$scope.setPage = function(page){
-		$scope.pageSize=30
-		$scope.currentPage = page
-		$location.hash(page)
-		$window.scrollTo(0,0)
-	}
-	$scope.getNumber = function(num) {
-	    return new Array(num)
-	}
-	$window.onload = function(){
-		$scope.loaded = true
-		$scope.$digest()
-		console.timeEnd('ng')
-	}
-	$scope.remove = function(img){
-		$scope.imgList = $scope.imgList.filter(function(i){
-			if(i === img){
-				return false
-			}
-			return true
-		});
-		localStorage.favList = JSON.stringify($scope.imgList)
-	}
-})
-
-
-
-
-
-
+window.oncontextmenu = function() {
+    return false
+}
+var FavListModule = angular.module("fav-list", [])
+    .controller("favList", function($scope, $window, $location) {
+        $scope.imgList = JSON.parse(localStorage.favList).reverse()
+        $scope.pageSize = 30 //每页的数量
+        $scope.pageNum = parseInt($scope.imgList.length / $scope.pageSize + 0.5)
+        $scope.currentPage = 1 //$location.hash()||$location.hash(1)
+        $scope.setPage = function(page) {
+            $scope.pageSize = 30
+            $scope.currentPage = page
+            $location.hash(page)
+            $window.scrollTo(0, 0)
+        }
+        $scope.getNumber = function(num) {
+            return new Array(num)
+        }
+        $window.onload = function() {
+            $scope.loaded = true
+            $scope.$digest()
+        }
+        $scope.remove = function(img) {
+            $scope.imgList = $scope.imgList.filter(function(i) {
+                if (i === img) {
+                    return false
+                }
+                return true
+            });
+            localStorage.favList = JSON.stringify($scope.imgList)
+        }
+        $scope.getImgUrl = function(img){
+            var imgId = img.match(/org\/([0-9]*)\//)[1]
+            return "http://500px.com/photo/"+imgId;
+        }
+    })
 
 
 
@@ -59,24 +58,24 @@ var FavListModule = angular.module("fav-list",[])
 
 
 FavListModule
-.filter('limit', function(){
-    return function (input, left, right) {
-        if(left==undefined)return input;
-        if(right==undefined){
-            right = left;
-            left =  0;
-        }
-        return input.slice(left,right);
-    };
-})
-.filter('pager',function($filter){
-    return function(input,pageNum,pageSize){
-        if(!angular.isArray(input))return input;
-        pageSize = pageSize||5;
-        pageNum = pageNum<1?1:pageNum;
-        return $filter('limit')(input,(pageNum-1)*pageSize,pageNum*pageSize);
-    };
-})
+    .filter('limit', function() {
+        return function(input, left, right) {
+            if (left == undefined) return input;
+            if (right == undefined) {
+                right = left;
+                left = 0;
+            }
+            return input.slice(left, right);
+        };
+    })
+    .filter('pager', function($filter) {
+        return function(input, pageNum, pageSize) {
+            if (!angular.isArray(input)) return input;
+            pageSize = pageSize || 5;
+            pageNum = pageNum < 1 ? 1 : pageNum;
+            return $filter('limit')(input, (pageNum - 1) * pageSize, pageNum * pageSize);
+        };
+    })
 // .filter('pager',function(limitFilter){
 //     return function(input,pageNum,pageSize){
 //         if(!angular.isArray(input))return input;
